39a45fdbc634a191839484723dbb33cb6139d576
yanjianhua <yanisok@163.com>
2017-06-23 22:29:24 +0800

    add file test2

---
+++ testgit/src/test2.java
@@ -0,0 +1,39 @@
+/*
+ * @(#)test2.java	1.0 2017年6月23日 下午10:29:02
+ *
+ * Copyright 2004-2010 Client Server International, Inc. All rights reserved.
+ * CSII PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * {type specification, must edit}
+ *
+ * @author  yanjianhua {must edit, use true name}
+ * <p>
+ *   Created on 2017年6月23日
+ *   Modification history	
+ *   {add your history}
+ * </p>
+ * <p>
+ *   IBS Product Expert Group, CSII
+ *   Powered by CSII PowerEngine 6.0
+ * </p>
+ * @version 1.0
+ * @since 1.0
+ */
+public class test2 {
+
+	/**
+	 * {method specification, must edit}
+	 *
+	 * @param args
+	 *
+	 * @version 1.0
+	 * @since 1.0
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
