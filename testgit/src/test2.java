c0c1c91bd2a18c4e1e19427858c3aa23c3efa5e7
yanisok <yanisok@163.com>
2017-06-23 22:52:20 +0800

    Update test2.java

--- testgit/src/test2.java
+++ testgit/src/test2.java
@@ -1,9 +1,4 @@
-/*
- * @(#)test2.java	1.0 2017年6月23日 下午10:29:02
- *
- * Copyright 2004-2010 Client Server International, Inc. All rights reserved.
- * CSII PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
+
 
 
 public class test2 {
